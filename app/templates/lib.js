13a8095aa98daaeb19b98da5111ab504abcd63a9
Cory Mawhorter <cory.mawhorter@gmail.com>
2014-08-11 12:06:13 -0700

    lib template enhancements

--- app/templates/lib.js
+++ app/templates/lib.js
@@ -1,21 +1,24 @@
-(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define([], function () {
-      return factory();
-    });
-  } else if (typeof exports !== 'undefined') {
-    module.exports = factory();
-  } else {
-    root.<%= name %> = factory();
-  }
+(function() {
+  var scope = this;
+  (function (global, factory) {
+    /*global define*/
+    if (typeof define === 'function' && define.amd) {
+      define([], function () {
+        return factory(global);
+      });
+    } else if (typeof exports !== 'undefined') {
+      module.exports = factory(global);
+    } else {
+      global.<%= name %> = factory(global);
+    }
+  }(scope, function () { // function (global) { if you need noConflict access or something
+    'use strict';
 
-}(this, function () {
-  'use strict';
+    // code goes here
+    var <%= name %> = function (options) {
 
-  // code goes here
-  var <%= name %> = function (options) {
+    };
 
-  };
-
-  return <%= name %>;
-}));
+    return <%= name %>;
+  }));
+})();
